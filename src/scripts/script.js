55667c31ea5b02f3add6eed4622941eadbb21cde
Nouval Rizky <nouvalr@gmail.com>
2025-03-31 19:25:29 +0800

    feat: toggle theme mode in header

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -16,3 +16,20 @@ if (navButton && navIcon && mobileNav) {
     }
   });
 }
+
+const themeButton = document.querySelector("#theme-button");
+const themeIcon = document.querySelector("#theme-icon");
+
+if (themeButton && themeIcon) {
+  themeButton.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.remove("dark");
+      themeIcon.classList.remove("fa-moon");
+      themeIcon.classList.add("fa-sun");
+    } else {
+      document.body.classList.add("dark");
+      themeIcon.classList.remove("fa-sun");
+      themeIcon.classList.add("fa-moon");
+    }
+  })
+}
