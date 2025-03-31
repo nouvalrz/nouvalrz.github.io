b618a1f7075f3e6babe15afab6d558cbac738211
Nouval Rizky <nouvalr@gmail.com>
2025-03-31 17:45:19 +0800

    feat: responsive nav

---
+++ src/scripts/script.js
@@ -0,0 +1,18 @@
+const navButton = document.querySelector("#nav-button");
+const navIcon = document.querySelector("#nav-icon");
+const mobileNav = document.querySelector("#mobile-nav");
+
+if (navButton && navIcon && mobileNav) {
+  navButton.addEventListener("click", () => {
+    if (navIcon.classList.contains("fa-bars")) {
+      navIcon.classList.remove("fa-bars");
+      navIcon.classList.add("fa-xmark");
+      mobileNav.classList.remove("translate-y-full");
+
+    } else {
+      navIcon.classList.remove("fa-xmark");
+      navIcon.classList.add("fa-bars");
+      mobileNav.classList.add("translate-y-full");
+    }
+  });
+}
